14f6037d91196a66cbea5d83895d48ba7d9ca4c7
Jtavora <jtavora09@gmail.com>
2023-08-04 18:26:03 -0300

    Adicionando Input field para o no do usuario

--- vrtk-assets/Assets/Scripts/DuplicateNode.cs
+++ vrtk-assets/Assets/Scripts/DuplicateNode.cs
@@ -1,76 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using TMPro;
-
-// public class DuplicateNode : MonoBehaviour
-// {
-//     public GameObject node;
-//     public TMP_Text textComponent;
-
-//     private Vector3 firstNodeVector;
-//     private Vector3 lastNodeVector;
-//     private int textCounter = 1;
-//     private bool isAdding = true;
-//     private List<GameObject> containers = new List<GameObject>();
-//     private Vector3 initialPosition; // Variável para armazenar a posição inicial de criação dos containers
-
-//     void Start()
-//     {
-//         firstNodeVector = new Vector3(node.transform.position.x,
-//             node.transform.position.y, node.transform.position.z);
-//         lastNodeVector = new Vector3(firstNodeVector.x, firstNodeVector.y, firstNodeVector.z);
-//         initialPosition = lastNodeVector; // Armazenar a posição inicial
-//     }
-
-//     void Update()
-//     {
-//         if (Input.GetKeyDown(KeyCode.T))
-//         {
-//             if (textCounter == 6)
-//             {
-//                 RemovePreviousContainers();
-//                 textCounter = 1; // Reiniciar o contador para 1 após atingir 6
-//                 isAdding = true; // Retomar a duplicação normal
-//                 lastNodeVector = initialPosition; // Voltar à posição inicial de criação
-//             }
-
-//             lastNodeVector.x -= isAdding ? 0.659f : -0.659f;
-
-//             // Criar novo objeto "container"
-//             GameObject newContainer = new GameObject("Container" + textCounter);
-//             newContainer.transform.position = lastNodeVector;
-
-//             // Criar novo nó com o texto atualizado como filho do objeto "container"
-//             GameObject newNode = Instantiate(node, newContainer.transform);
-//             newNode.transform.localPosition = Vector3.zero; // Defina a posição do nó localmente para (0, 0, 0)
-//             newNode.name = "Node" + textCounter;
-
-//             TMP_Text newTextComponent = newNode.GetComponentInChildren<TMP_Text>();
-//             if (newTextComponent != null)
-//             {
-//                 newTextComponent.text = textCounter.ToString();
-//             }
-
-//             // Adicionar o novo objeto "container" na lista
-//             containers.Add(newContainer);
-
-//             // Incrementar o contador
-//             textCounter++;
-//         }
-//     }
-
-//     // Remover os objetos anteriores criados, mas manter a lista de containers vazia para reiniciar a duplicação
-//     private void RemovePreviousContainers()
-//     {
-//         foreach (GameObject container in containers)
-//         {
-//             Destroy(container);
-//         }
-//         containers.Clear();
-//     }
-// }
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -79,12 +6,12 @@ using TMPro;
 public class DuplicateNode : MonoBehaviour
 {
     public GameObject node;
-    public TMP_Text textComponent;
+    public TMP_InputField userInputField; // Referência ao InputField do Unity onde o usuário digita o texto
 
     private Vector3 firstNodeVector;
     private Vector3 lastNodeVector;
     private int textCounter = 1;
-    private List<GameObject> containers = new List<GameObject>();
+    private List<GameObject> nodeList = new List<GameObject>();
     private Vector3 initialPosition; // Variável para armazenar a posição inicial de criação dos containers
 
     void Start()
@@ -97,53 +24,56 @@ public class DuplicateNode : MonoBehaviour
 
     void Update()
     {
+        // Verificar se o usuário pressionou "T" para criar o nó
         if (Input.GetKeyDown(KeyCode.T))
         {
-            if (textCounter == 7)
+            CreateNewNode();
+        }
+    }
+
+    private void CreateNewNode()
+    {
+        if (textCounter >= 7)
+            ResetNodes(); // Remover todos os nós antes de reiniciar
+
+        GameObject newNode = Instantiate(node, lastNodeVector, Quaternion.identity);
+        newNode.SetActive(true);
+        newNode.transform.parent = transform;
+        newNode.name = "Node" + textCounter;
+
+        TMP_Text newTextComponent = newNode.GetComponentInChildren<TMP_Text>();
+        if (newTextComponent != null)
+        {
+            if (!string.IsNullOrEmpty(userInputField.text))
             {
-                if (containers.Count > 0)
-                {
-                    RemoveAllContainers(); // Remover todos os containers antes de reiniciar
-                }
-                textCounter = 1; // Reiniciar o contador para 1
-                lastNodeVector = initialPosition; // Voltar à posição inicial de criação
+                newTextComponent.text = userInputField.text;
+                userInputField.text = ""; // Limpar o input após atribuir o texto ao nó
             }
-
-            // Criar novo objeto "container"
-            GameObject newContainer = new GameObject("Node" + textCounter);
-            newContainer.transform.position = lastNodeVector;
-
-            // Criar novo nó com o texto atualizado como filho do objeto "container"
-            GameObject newNode = Instantiate(node, newContainer.transform);
-            newNode.transform.localPosition = Vector3.zero; // Defina a posição do nó localmente para (0, 0, 0)
-            newNode.name = "Node" + textCounter;
-
-            TMP_Text newTextComponent = newNode.GetComponentInChildren<TMP_Text>();
-            if (newTextComponent != null)
+            else
             {
                 newTextComponent.text = textCounter.ToString();
             }
-
-            // Adicionar o novo objeto "container" na lista
-            containers.Add(newContainer);
-
-            // Atualizar a posição para a próxima criação
-            lastNodeVector.x -= 0.659f;
-
-            // Incrementar o contador
-            textCounter++;
         }
+
+        // Adicionar o novo objeto "container" na lista
+        nodeList.Add(newNode);
+
+        // Atualizar a posição para a próxima criação
+        lastNodeVector.x -= 0.659f;
+
+        // Incrementar o contador
+        textCounter++;
     }
 
-    // Remover todos os objetos criados anteriormente e limpar a lista de containers
-    private void RemoveAllContainers()
+    // Remover todos os objetos criados anteriormente e limpar a lista de nós
+    private void ResetNodes()
     {
-        foreach (GameObject container in containers)
+        foreach (GameObject node in nodeList)
         {
-            Destroy(container);
+            Destroy(node);
         }
-        containers.Clear();
+        nodeList.Clear();
+        textCounter = 1; // Reiniciar o contador para 1
+        lastNodeVector = initialPosition; // Voltar à posição inicial de criação
     }
 }
-
-
